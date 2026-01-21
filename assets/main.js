3cbce8fec8b4d82df8cd8938f11c3db2211c3ba1
lyfedev-csharp <xclipse13295@gmail.com>
2026-01-21 20:17:15 -0330

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,13 @@
 document.querySelectorAll(".card").forEach((card, i) => {
   card.style.animationDelay = `${i * 0.18}s`;
 });
+
+const glow = document.querySelector('.glow-bg');
+
+document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    
+    glow.style.left = x + 'px';
+    glow.style.top = y + 'px';
+});
